13a21a5b4bbcad08b003bb471d7b656bef87166d
ayuan9 <1419437103@qq.com>
2025-08-06 11:10:46 +0800

    Delete api directory

--- api/status.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, Response
-
-app = Flask(__name__)
-
-@app.route("/", methods=["GET"])
-def handler():
-    return Response('{"message": "hello world"}', content_type="application/json")
-
-handler = app
